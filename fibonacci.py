890a68860a9d3149784baf5160fd584181a2a12d
Eli <etsuji1107@gmail.com>
2023-04-04 22:47:57 -0400

    Fixed fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+import random
+
+length_to_create = input("How many Fibonacci numbers do you want to create? ")
+length_to_create = int(length_to_create)
+
+fibonacci_list = [1, 1]
+
+for i in range(length_to_create -2):
+    fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
+
+print(fibonacci_list)
+
+#Main
