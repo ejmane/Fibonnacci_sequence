e918df37ffacc35406daaeb5468dfe1d7f138953
Eli <etsuji1107@gmail.com>
2023-04-04 22:01:38 -0400

    Changed method of grabbing last 2 numbers in list to using the -2 index

--- Fibonacci.py
+++ Fibonacci.py
@@ -6,7 +6,7 @@ length_to_create = int(length_to_create)
 fibonacci_list = [1, 1]
 
 for i in range(length_to_create -2):
-    fibonacci_list.append(fibonacci_list[len(fibonacci_list) -1] + fibonacci_list[len(fibonacci_list) -2])
+    fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
 
 print(fibonacci_list)
 
